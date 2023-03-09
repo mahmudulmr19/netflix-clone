110f7b8fe44066d207317cd3a471198ee0cebd6f
Mahmudulmr19 <mahmudulmr19@gmail.com>
2023-03-09 13:28:04 +0600

    error handlings

--- pages/home.tsx
+++ pages/home.tsx
@@ -21,7 +21,7 @@ export async function getServerSideProps(context: NextPageContext) {
   };
 }
 
-export default function home() {
+export default function Home() {
   const { data: user } = useCurrentUser();
 
   return (
